7d1c16ffdfe1e8d51eab2775c3a1c459a9dceec9
orieasy1 <easy1nhard2@gmail.com>
2025-01-10 21:01:57 +0900

    inital setting

---
+++ src/main/java/com/storder/order/StorderApplication.java
@@ -0,0 +1,13 @@
+package com.storder.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorderApplication.class, args);
+    }
+
+}
