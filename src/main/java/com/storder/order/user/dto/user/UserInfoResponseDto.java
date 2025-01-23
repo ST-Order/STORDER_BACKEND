d7931630690c0325f58bdda29424a9324040032d
heogeonho <geonh1016@gmail.com>
2025-01-23 20:16:23 +0900

    feat: 마이페이지 API 컨트롤러 및 DTO 작성 및 Swagger 작업

---
+++ src/main/java/com/storder/order/user/dto/user/UserInfoResponseDto.java
@@ -0,0 +1,21 @@
+package com.storder.order.user.dto.user;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class UserInfoResponseDto {
+
+    @Schema(description = "사용자 이름", example = "가나디")
+    private String name;
+
+    @Schema(description = "6개월 간 총 구매 금액", example = "19000")
+    private int totalOrderPrice;
+
+    @Schema(description = "6개월 간 총 구매 횟수", example = "3")
+    private int totalOrderCount;
+}
