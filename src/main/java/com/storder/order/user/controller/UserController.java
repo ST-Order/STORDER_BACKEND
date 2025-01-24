c72dda7cd8c06a7aaf39244021f29c28a0364a73
heogeonho <geonh1016@gmail.com>
2025-01-24 12:02:37 +0900

    feat: UserController 응답 값 ApiResponse 활용하도록 수정

--- src/main/java/com/storder/order/user/controller/UserController.java
+++ src/main/java/com/storder/order/user/controller/UserController.java
@@ -27,17 +27,20 @@ public class UserController {
             summary = "사용자 이름과 6개월 구매 통계 조회",
             description = "사용자의 이름과 해당 사용자 6개월간 구매 금액 및 횟수를 조회합니다.")
     @GetMapping("/mypage")
-    public UserInfoResponseDto getMyPageSummary() {
-        return UserInfoResponseDto.builder()
-                .name("가다니")
-                .totalOrderPrice(19000)
-                .totalOrderCount(3)
-                .build();
+    public ApiResponse<UserInfoResponseDto> getMyPageSummary() {
+        UserInfoResponseDto userInfo =
+                UserInfoResponseDto.builder()
+                        .name("가다니")
+                        .totalOrderPrice(19000)
+                        .totalOrderCount(3)
+                        .build();
+
+        return ApiResponse.success("사용자 정보 조회에 성공하였습니다.", userInfo);
     }
 
     @Operation(summary = "주문 내역 조회", description = "주문 내역을 조회합니다.")
     @GetMapping("/mypage/orders/list")
-    public OrderResponseDto getOrderList(
+    public ApiResponse<OrderResponseDto> getOrderList(
             @RequestParam @Parameter(description = "조회할 월", example = "2024-10") String month) {
         List<OrderResponseDto.OrderDto> orders =
                 List.of(
@@ -63,14 +66,14 @@ public class UserController {
                                 .price(9000)
                                 .build());
 
-        return OrderResponseDto.builder().orders(orders).build();
+        return ApiResponse.success(
+                "주문 내역 조회에 성공하였습니다.", OrderResponseDto.builder().orders(orders).build());
     }
 
     @Operation(summary = "주문 상세 조회", description = "특정 주문에 대한 상세 정보를 조회합니다.")
     @GetMapping("/mypage/orders/{orderId}/details")
-    public OrderDetailResponseDto getOrderDetails(
+    public ApiResponse<OrderDetailResponseDto> getOrderDetails(
             @PathVariable @Parameter(description = "주문 번호", example = "443") int orderId) {
-        // 더미 데이터 예시
         List<OrderDetailResponseDto.MenuDto> menu =
                 List.of(
                         OrderDetailResponseDto.MenuDto.builder()
@@ -92,21 +95,23 @@ public class UserController {
                                                         .build()))
                                 .build());
 
-        return OrderDetailResponseDto.builder()
-                .orderId(orderId)
-                .orderTime(LocalDateTime.of(2024, 10, 4, 18, 3, 0, 0))
-                .storeName("경성카츠")
-                .menu(menu)
-                .totalPrice(18000)
-                .paymentMethod("KAKAOPAY")
-                .paymentTime(LocalDateTime.of(2024, 10, 4, 18, 2, 30, 0))
-                .build();
+        OrderDetailResponseDto orderDetail =
+                OrderDetailResponseDto.builder()
+                        .orderId(orderId)
+                        .orderTime(LocalDateTime.of(2024, 10, 4, 18, 3, 0, 0))
+                        .storeName("경성카츠")
+                        .menu(menu)
+                        .totalPrice(18000)
+                        .paymentMethod("KAKAOPAY")
+                        .paymentTime(LocalDateTime.of(2024, 10, 4, 18, 2, 30, 0))
+                        .build();
+
+        return ApiResponse.success("주문 상세 조회에 성공하였습니다.", orderDetail);
     }
 
     @Operation(summary = "리뷰 작성", description = "사용자가 메뉴에 대한 리뷰를 작성합니다.")
     @PostMapping("/reviews")
-    public ApiResponse createReview(@RequestBody ReviewRequestDto reviewRequestDto) {
-
-        return null;
+    public ApiResponse<Void> createReview(@RequestBody ReviewRequestDto reviewRequestDto) {
+        return ApiResponse.success("리뷰 작성에 성공하였습니다.", null);
     }
 }
