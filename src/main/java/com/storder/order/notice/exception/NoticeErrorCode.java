5423bf54f94165ec0f75f3a377db973056be96a9
orieasy1 <easy1nhard2@gmail.com>
2025-01-30 16:39:35 +0900

    Feautre: Notice 도메인 에러정의

--- src/main/java/com/storder/order/notice/exception/NoticeErrorCode.java
+++ src/main/java/com/storder/order/notice/exception/NoticeErrorCode.java
@@ -17,7 +17,26 @@ public enum NoticeErrorCode implements BaseErrorCode {
     INVALID_NOTICE_CATEGORY(BAD_REQUEST, "NOTICE_400_1", "잘못된 공지사항 카테고리입니다."),
 
     @ExplainError("공지사항 노출 시간이 유효하지 않을 경우 발생하는 오류입니다.")
-    INVALID_NOTICE_EXPOSURE_TIME(BAD_REQUEST, "NOTICE_400_2", "잘못된 공지사항 노출 시간입니다.");
+    INVALID_NOTICE_EXPOSURE_TIME(BAD_REQUEST, "NOTICE_400_2", "잘못된 공지사항 노출 시간입니다."),
+
+    @ExplainError("공지사항 제목이 비어있을 경우 발생하는 오류입니다.")
+    EMPTY_NOTICE_TITLE(BAD_REQUEST, "NOTICE_400_3", "공지사항 제목이 비어있습니다."),
+
+    @ExplainError("공지사항 내용이 비어있을 경우 발생하는 오류입니다.")
+    EMPTY_NOTICE_CONTENT(BAD_REQUEST, "NOTICE_400_4", "공지사항 내용이 비어있습니다."),
+
+    @ExplainError("이미 삭제된 공지사항을 다시 삭제하려할 때 발생하는 오류입니다.")
+    ALREADY_DELETED_NOTICE(BAD_REQUEST, "NOTICE_400_5", "이미 삭제된 공지사항입니다."),
+
+    @ExplainError("로그인한 사용자가 해당 공지사항을 수정할 권한이 없을 때 발생하는 오류입니다.")
+    NOT_NOTICE_OWNER(FORBIDDEN, "NOTICE_403_1", "해당 공지사항의 수정 권한이 없습니다."),
+
+    @ExplainError("로그인한 사용자가 해당 공지사항을 삭제할 권한이 없을 때 발생하는 오류입니다.")
+    NOT_NOTICE_OWNER_DELETE(FORBIDDEN, "NOTICE_403_2", "해당 공지사항의 삭제 권한이 없습니다."),
+
+    @ExplainError("공지사항이 존재하지 않을 경우 발생하는 오류입니다.")
+    NOTICE_NOT_FOUND(NOT_FOUND, "NOTICE_404_1", "해당 공지사항이 존재하지 않습니다.");
+
 
     private final Integer status;
     private final String code;
